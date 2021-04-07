979bff80e3a3e77e6faa74aad32ff7bcb0c726e9
Nolan Wang <vitanuan@gmail.com>
2021-04-07 15:58:12 +0100

    atlas link

--- server/db.js
+++ server/db.js
@@ -1,3 +1,3 @@
 let mongoose = require("mongoose");
-let url = "mongodb://127.0.0.1:27017/emotion";
+let url = "mongodb+srv://dbDev:dbDev@cluster0.h3e49.mongodb.net/myFirstDatabase?retryWrites=true&w=majorityt";
 let db = mongoose.connect(url);
