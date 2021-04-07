23d07354d2812ec56757887e080d8b399eaece9f
Nolan Wang <vitanuan@gmail.com>
2021-04-07 18:00:31 +0100

    add atlas config

--- server/db.js
+++ server/db.js
@@ -1,3 +1,4 @@
 let mongoose = require("mongoose");
-let url = "mongodb+srv://dbDev:dbDev@cluster0.h3e49.mongodb.net/myFirstDatabase?retryWrites=true&w=majorityt";
-let db = mongoose.connect(url);
+let url = process.env.MONGODB_URI;
+let db = mongoose.connect(url,{useNewUrlParser: true, useUnifiedTopology: true});
+
